04ce1448b0739056eac936997f5efbac347901c0
Manuel Garces <manuel.garces971@gmail.com>
2026-04-06 22:57:50 -0700

    IE1 Complete through step 10

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,26 +1,10 @@
 
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor" // the rest of the data
-    },
-    {
-      name: "Ronny",
-      job: "Electrician" // the rest of the data
-    },
-    {
-      name: "Elmo",
-      job: "Puppet" // the rest of the data
-    },
-    {
-      name: "Rocco",
-      job: "Rock" // the rest of the data
-    },
-  ]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -29,14 +13,20 @@ function MyApp() {
     setCharacters(updated);
   }
 
+  function updateList(person) {
+  setCharacters([...characters, person]);
+  }
+
   return (
-    <div className="container">
+  <div className="container">
     <Table
       characterData={characters}
       removeCharacter={removeOneCharacter}
+      
     />
+    <Form handleSubmit={updateList} />
   </div>
-  );
+);
 }
 
 
