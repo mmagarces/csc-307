0bf52f9858a3db292985a6222d411145f2f9bb31
Manuel Garces <manuel.garces971@gmail.com>
2026-04-13 11:41:15 -0700

    Finished IE2

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -40,16 +40,23 @@ const findUserByName = (name) => {
     (user) => user["name"] === name
   );
 };
-
+const findUserByNameAndJob = (name, job) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name && user["job"] === job);
+};
 app.get("/users", (req, res) => {
-  const name = req.query.name;
-  if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
+  const {name, job} = req.query;
+
+  let result;
+  if (name !== undefined && job !== undefined) {
+    result = findUserByNameAndJob(name, job);
+  } else if (name !== undefined) {
+    result = findUserByName(name);
   } else {
-    res.send(users);
+    result = users["users_list"];
   }
+
+  res.send({ users_list: result });
 });
 
 const findUserById = (id) =>
@@ -64,6 +71,33 @@ app.get("/users/:id", (req, res) => {
     res.send(result);
   }
 });
+
+
+const deleteUserById = (id) => {  //Here we should use find index instead of find, due to getting object with find
+  const index = users["users_list"].findIndex(
+    (user) => user["id"] === id
+  );
+  if (index === -1) {
+    return null; // not found
+  }
+  //here we then grab the user based on the index of the object
+  const deletedUser = users["users_list"][index];
+  users["users_list"].splice(index, 1);
+
+  return deletedUser;
+};
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const deletedUser = deleteUserById(id);
+  if (deletedUser === null) {
+    res.status(404).send("User not found");
+  } else {
+    res.send(deletedUser);
+  }
+});
+
+
 
 const addUser = (user) => {
   users["users_list"].push(user);
