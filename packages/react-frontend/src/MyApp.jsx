30ce6f982c9cf01f95369580c9ff824767b6cc6d
Manuel Garces <manuel.garces971@gmail.com>
2026-04-15 11:40:33 -0700

    added frontend to backend connecting logic

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -28,9 +28,26 @@ function MyApp() {
       });
   }, []);
 
-  function updateList(person) {
-  setCharacters([...characters, person]);
-  }
+  
+function postUser(person) {
+  const promise = fetch("Http://localhost:8000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(person),
+  });
+
+  return promise;
+}
+
+function updateList(person) {
+  postUser(person)
+    .then(() => setCharacters([...characters, person]))
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
   return (
   <div className="container">
